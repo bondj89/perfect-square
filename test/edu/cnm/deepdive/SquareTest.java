5779ee759e2d3b1f0a8776541bf0e4338aed9d92
Bond <jobocuban@gmail.com>
2019-09-26 09:52:11 -0600

    Used a Lambda.

--- test/edu/cnm/deepdive/SquareTest.java
+++ test/edu/cnm/deepdive/SquareTest.java
@@ -20,12 +20,7 @@ class SquareTest {
 
   @Test
   void isPerfectSquareException() {
-    assertThrows(IllegalArgumentException.class, new Executable() {
-      @Override
-      public void execute() throws Throwable {
-        Square.isPerfectSquare(-1);
-      }
-    });
+    assertThrows(IllegalArgumentException.class, () -> Square.isPerfectSquare(-1));
   }
 
 }
